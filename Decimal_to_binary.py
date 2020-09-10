ec6819fc81cfc735e86e874715fc9d755d59f0fa
Abishek V <37650145+Abicreepzz@users.noreply.github.com>
2020-09-10 17:46:14 +0530

    Update Decimal_to_binary.py

--- Decimal_to_binary.py
+++ Decimal_to_binary.py
@@ -6,3 +6,14 @@ def binary(n):
   return int(result)
 
 binary(5) ##output= 101
+# Another simple one line code for converting the decimal number to binary is followed:
+
+def f(n): print('{:04b}'.format(n))
+  
+binary(5) ##output =101
+
+# If we want to print in the 8 bit digit of the binary numbers then we can just specify the 8 bit in function.
+
+def f(n): print('{:08b}'.format(n))
+  
+binary(5) ##output = 00000101
